a1be16c21c8016683cb7da1ce300f8a7c8e42be1
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2020-07-16 10:43:53 +0700

    Golang Fundamental

--- basic_golang/map/main.go
+++ basic_golang/map/main.go
@@ -15,9 +15,26 @@ func main() {
 	// fmt.Println(myMap["ruby"])
 
 	myMap := map[string]string{
-		"ruby": "is beautiful",
-		"go":   "is super fast",
+		"ruby":       "is beautiful",
+		"go":         "is super fast",
+		"javascript": "is hype",
 	}
 
 	fmt.Println(myMap)
+	for key, val := range myMap {
+		fmt.Println("Key : ", key, "Value : ", val)
+	}
+
+	fmt.Println("=========================")
+
+	delete(myMap, "ruby")
+	for key, val := range myMap {
+		fmt.Println("Key : ", key, "Value : ", val)
+	}
+
+	fmt.Println("=========================")
+
+	value, isAvailable := myMap["python"]
+	fmt.Println(isAvailable)
+	fmt.Println(value)
 }
