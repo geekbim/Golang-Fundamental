12de9924496602ccf722eec7b5ee55c4463bffaa
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2020-08-29 14:56:10 +0700

    Golang Fundamental

--- basic_golang/pointer/main.go
+++ basic_golang/pointer/main.go
@@ -16,15 +16,30 @@ func main() {
 	// fmt.Println(numberA)
 	// fmt.Println(*numberB)
 
-	var numberA int = 5
-	var numberB *int = &numberA
+	// var numberA int = 5
+	// var numberB *int = &numberA
 
-	fmt.Println(numberA)
-	fmt.Println(numberB)
-	fmt.Println(*numberB)
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
 
-	numberA = 20
-	fmt.Println(numberA)
-	fmt.Println(numberB)
-	fmt.Println(*numberB)
+	// numberA = 20
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
+
+	number := 5
+	fmt.Println("Alamat memory : ", &number)
+	fmt.Println("Nilai Awal : ", number)
+
+	change(&number, 100)
+
+	fmt.Println("Nilai Akhir : ", number)
+	fmt.Println("Alamat Memory : ", &number)
+}
+
+func change(old *int, new int) {
+	*old = new
+	fmt.Println("Alamat memory : ", *old)
+	fmt.Println("Di dalam function : ", old)
 }
